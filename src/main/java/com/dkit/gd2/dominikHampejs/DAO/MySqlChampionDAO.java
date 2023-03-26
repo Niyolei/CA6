e48d946d371ad1b3e3a65eda6f4c97813c05d0b1
Niyolei <113349382+Niyolei@users.noreply.github.com>
2023-03-26 10:29:16 +0100

    Delete Chamption
    The code deletes a champion from the database based on the id.

--- src/main/java/com/dkit/gd2/dominikHampejs/DAO/MySqlChampionDAO.java
+++ src/main/java/com/dkit/gd2/dominikHampejs/DAO/MySqlChampionDAO.java
@@ -3,6 +3,8 @@ package com.dkit.gd2.dominikHampejs.DAO;
 import com.dkit.gd2.dominikHampejs.DTO.Champion;
 import com.dkit.gd2.dominikHampejs.Exceptions.DAOexception;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class MySqlChampionDAO extends MySqlDAO implements IChampionDAO {
@@ -19,7 +21,30 @@ public class MySqlChampionDAO extends MySqlDAO implements IChampionDAO {
 
     @Override
     public boolean deleteChampion(int id) throws DAOexception {
-        return false;
+        Connection con = null;
+        PreparedStatement ps = null;
+        int result = 0;
+
+        try {
+con = this.getConnection();
+            String query = "DELETE FROM champion WHERE id = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, id);
+            result = ps.executeUpdate();
+        } catch (Exception e) {
+            throw new DAOexception("deleteChampion() " + e.getMessage());
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                this.freeConnection(con);
+            } catch (Exception e) {
+                throw new DAOexception("deleteChampion() " + e.getMessage());
+            }
+
+        }
+        return result == 1;
     }
 
     @Override
