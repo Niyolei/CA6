9600ba90250f559cfbd904b7d111483b365a4c72
Niyolei <113349382+Niyolei@users.noreply.github.com>
2023-03-26 10:23:59 +0100

    Find By Id
    The code accesses the database and returns the champion based on id.

--- src/main/java/com/dkit/gd2/dominikHampejs/DAO/MySqlChampionDAO.java
+++ src/main/java/com/dkit/gd2/dominikHampejs/DAO/MySqlChampionDAO.java
@@ -3,6 +3,9 @@ package com.dkit.gd2.dominikHampejs.DAO;
 import com.dkit.gd2.dominikHampejs.DTO.Champion;
 import com.dkit.gd2.dominikHampejs.Exceptions.DAOexception;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
 public class MySqlChampionDAO extends MySqlDAO implements IChampionDAO {
@@ -14,7 +17,40 @@ public class MySqlChampionDAO extends MySqlDAO implements IChampionDAO {
 
     @Override
     public Champion findChampionById(int id) throws DAOexception {
-        return null;
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Champion c = null;
+
+        try{
+            con = this.getConnection();
+            String query = "SELECT * FROM champion WHERE id = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                String name = rs.getString("name");
+                String role = rs.getString("role");
+                double winRate = rs.getDouble("winRate");
+                c = new Champion(id, name, role, winRate);
+            }
+        } catch (Exception e) {
+            throw new DAOexception("findChampionById() " + e.getMessage());
+        } finally {
+            try{
+                if(rs != null){
+                    rs.close();
+                }
+                if(ps != null){
+                    ps.close();
+                }
+                this.freeConnection(con);
+            } catch (Exception e) {
+                throw new DAOexception("findChampionById() " + e.getMessage());
+            }
+        }
+        return c;
     }
 
     @Override
