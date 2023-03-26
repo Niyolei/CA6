8ce880d79c1bb80a1d3f2061a5578c960cc18989
Niyolei <113349382+Niyolei@users.noreply.github.com>
2023-03-26 10:33:37 +0100

    Insert New Champion
    The code takes in a character instances and inputs it into the database.

--- src/main/java/com/dkit/gd2/dominikHampejs/DAO/MySqlChampionDAO.java
+++ src/main/java/com/dkit/gd2/dominikHampejs/DAO/MySqlChampionDAO.java
@@ -3,6 +3,8 @@ package com.dkit.gd2.dominikHampejs.DAO;
 import com.dkit.gd2.dominikHampejs.DTO.Champion;
 import com.dkit.gd2.dominikHampejs.Exceptions.DAOexception;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class MySqlChampionDAO extends MySqlDAO implements IChampionDAO {
@@ -24,6 +26,31 @@ public class MySqlChampionDAO extends MySqlDAO implements IChampionDAO {
 
     @Override
     public boolean insertChampion(Champion c) throws DAOexception {
-        return false;
+        Connection con = null;
+        PreparedStatement ps = null;
+        int result = 0;
+
+        try{
+            con = this.getConnection();
+            String query = "INSERT INTO champion (id, name, role, difficulty) VALUES (?, ?, ?, ?)";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, c.getId());
+            ps.setString(2, c.getName());
+            ps.setString(3, c.getRole());
+            ps.setDouble(4, c.getWinRate());
+            result = ps.executeUpdate();
+        } catch (Exception e) {
+            throw new DAOexception("insertChampion() " + e.getMessage());
+        } finally {
+            try{
+                if(ps != null){
+                    ps.close();
+                }
+                this.freeConnection(con);
+            } catch (Exception e) {
+                throw new DAOexception("insertChampion() " + e.getMessage());
+            }
+        }
+        return result == 1;
     }
 }
